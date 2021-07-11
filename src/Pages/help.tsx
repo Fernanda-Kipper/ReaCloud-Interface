a483c42453ed1e8518de3a10b6840f32c795f69c
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2021-07-11 14:27:35 -0300

    fix: refactoring help page

--- src/Pages/help.tsx
+++ src/Pages/help.tsx
@@ -16,39 +16,42 @@ function HelpPage() {
     <div className="help-content">
       <Header></Header>
       <main>
-          <h3>Passo a Passo de como salvar o recurso na nuvem</h3>
+          <h3>Passo a Passo de como salvar Recursos Educacionais na nuvem</h3>
           <ol>
               <li>
                 <h6>Acesse a plataforma Google Drive</h6>
-                <img src={landingImg} alt=""/>
+                <img src={landingImg} alt="Página inicial do drive"/>
               </li>
               <li>
                 <h6>Realize o Login</h6>
-                <img src={loginImg} alt=""/>
+                <img src={loginImg} alt="Login no drive"/>
               </li>
               <li>
-                <h6>Após login, você será redirecionado para seu drive, omde deve clicar em 'novo'</h6>
-                <img src={initialImg} alt=""/>
+                <h6>Após login, você será redirecionado para seu drive, onde deve clicar no botão 'novo'</h6>
+                <img src={initialImg} alt="Painel do drive"/>
               </li>
               <li>
-                <h6>Opções serão mostradas, então selecione a opção que se encaixa - pasta ou arquivo</h6>
-                <img src={uploadImg} alt=""/>
+                <h6>Opções serão mostradas, então selecione a opção que se encaixa - seu recurso é uma pasta ou apenas um arquivo</h6>
+                <img src={uploadImg} alt="Opções, upload de pasta ou arquivo"/>
               </li>
               <li>
-                <h6>Após fazer o upload do recurso, clique para vizualizá-lo e abra as configurações dele</h6>
-                <img src={viewImg} alt=""/>
+                <h6>Uma janela para você escolher o arquivo correto no seu computador será aberta, selecione o arquivo ou pasta deseja e clique em 'abrir'</h6>
               </li>
               <li>
-                <h6>Selecione a opção 'compartilhar'</h6>
-                <img src={optionsImg} alt=""/>
+                <h6>Após concluído o upload do recurso, clique para visualiza-lo e abra as configurações dele - 3 pontinhos no canto direito </h6>
+                <img src={viewImg} alt="Visualizando arquivo"/>
               </li>
               <li>
-                <h6>Certifique-se que o arquivo está disponível para aqueles que possuem o link</h6>
-                <img src={shareImg} alt=""/>
+                <h6>Selecione a opção 'Compartilhar'</h6>
+                <img src={optionsImg} alt="Selecionando a opção compartilhar"/>
               </li>
               <li>
-                <h6>Além disso, verifique se aqueles que possuem o link são apenas leitores, para não poderem alterar seu arquivo</h6>
-                <img src={finalImg} alt=""/>
+                <h6>Certifique-se que o arquivo está disponível para qualquer pessoa que possuir o link</h6>
+                <img src={shareImg} alt="Configurações de compartilhamento"/>
+              </li>
+              <li>
+                <h6>Além disso, verifique se aqueles que possuem o link são apenas leitores, para não possam alterar seu arquivo</h6>
+                <img src={finalImg} alt="Selecionando apenas leitores"/>
               </li>
               <li>
                 <h6>Por fim, copie o link disponibilizado e forneça ele no momento de cadastro do recurso</h6>
