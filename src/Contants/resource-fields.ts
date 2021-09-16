aabe7bb79feebc2802e4c87c378be79ac7e3d8b3
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2021-09-16 00:34:03 -0300

    fix(refactor-resource-page): refactor resource page to use constants fields

---
+++ src/Contants/resource-fields.ts
@@ -0,0 +1,19 @@
+import Resource from '../Interfaces/resource'
+
+export const RESOURCE_FIELDS = [
+  {name:'Autor' , field: (data: Resource) => data?.author ?? '', property: 'dc.author'},
+  {name:'Quando o recurso foi criado' , field: (data: Resource) => data?.date_of_publishment ?? '', property: 'dc.data'},
+  {name:'Descrição' , field: (data: Resource) => data?.description ?? '', property: 'dc.description'},
+  {name:'Público alvo' , field: (data: Resource) => data?.audience ?? '', property: 'dc.audience'},
+  {name:'Área do conhecimento' , field: (data: Resource) => data?.subject ?? '', property: 'dc.subject'},
+  {name:'Palavras chave' , field: (data: Resource) => data?.keywords ?? '', property: 'dc.data'},
+  {name:'Compartilhador por' , field: (data: Resource) => data?.userName ?? '', property: 'dc.created_by'},
+  {name:'Recurso correlato' , field: (resource: Resource) => resource?.relation ?? '', property: 'dc.relation'},
+  {name:'Ultima modificação na plataforma' , field: (resource: Resource) => resource?.last_modification ?? '', property: 'dc.last_modification'},
+  {name:'Tipo de recurso' , field: (resource: Resource) => resource?.type ?? '', property: 'dc.type'},
+  {name:'Formato do recurso' , field: (resource: Resource) => resource?.format ?? '', property: 'dc.format'},
+  {name:'Linguagem' , field: (resource: Resource) => resource?.language ?? '', property: 'dc.language'},
+  {name:'Contribuidores do recurso' , field: (resource: Resource) => resource?.contributor ?? '', property: 'dc.contributor'},
+  {name:'Local que o recurso foi publicado' , field: (resource: Resource) => resource?.publisher ?? '', property: 'dc.publisher'},
+  {name:'Pré-requisitos técnicos' , field: (resource: Resource) => resource?.technical_requirements ?? '', property: 'dc.technical_requirements'},
+]
