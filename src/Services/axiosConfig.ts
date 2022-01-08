5a5f0ed1e2ffb97076db6bb4f9d6209a1c69862b
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-01-08 18:39:31 -0300

    fix: axios config

--- src/Services/axiosConfig.ts
+++ src/Services/axiosConfig.ts
@@ -1,11 +1,10 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-    baseURL: 'http://localhost:3200',
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3200' : process.env.REACT_APP_API_URL,
     withCredentials: true,
     headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Content-Type': 'application/json'
     }
 })
 
