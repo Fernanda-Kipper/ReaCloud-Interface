392061aec55ad9139ee32475bc4f224b215fe60f
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-02-06 16:34:36 -0300

    feat: initiate google login logic

---
+++ src/Components/google-login-button.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import querystring from "querystring";
+
+import "../Styles/components/google-login-button.css";
+
+interface Props {
+  text: string
+}
+
+function getGoogleAuthURL() {
+  const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
+  const options = {
+    redirect_uri: "http://localhost:3000/auth",
+    client_id: "909104427753-uccm5f130u3pkukhngf8ng8pfeinc3tn.apps.googleusercontent.com",
+    access_type: "offline",
+    response_type: "code",
+    prompt: "consent",
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ].join(" "),
+  };
+
+  return `${rootUrl}?${querystring.stringify(options)}`;
+}
+
+export function GoogleLoginButton(props: Props){
+  const onClick = () => {
+    const authUrl = getGoogleAuthURL()
+    window.open(authUrl)
+  }
+
+  return(
+    <button onClick={onClick} className="google-button">
+      <FcGoogle/>
+      {props.text}
+    </button>
+  )
+}
--- src/Pages/login.tsx
+++ src/Pages/login.tsx
@@ -1,6 +1,5 @@
-import React, { FormEvent, useState, useContext } from 'react';
-import {Link, useHistory} from 'react-router-dom';
-import axios from '../Services/axiosConfig'
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import '../Styles/pages/login.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,48 +7,20 @@ import 'react-toastify/dist/ReactToastify.css';
 import LoadingBar from 'react-top-loading-bar'
 
 import { UserContext } from '../Context/UserContext';
-import { toast } from 'react-toastify';
-import { TextInput } from '../Components/form/text-input';
-import { DefaultButton } from '../Components/default-button';
+import { GoogleLoginButton } from '../Components/google-login-button';
 
 
 function LoginPage() {
   const history = useHistory()
   const [progress, setProgress] = useState(0)
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
   const {setValue, setName} = useContext(UserContext)
 
-  async function handleLogin(event: FormEvent){
-        event.preventDefault()
+  const handleGoogleLogin = () => {
 
-        setProgress(progress + 50)
-
-        const Formdata = {email: email, password: password}
-
-        await axios.post('/login', Formdata, {
-          onUploadProgress: (event) => {
-            setProgress(event.loaded)
-        }})
-        .then(res => {
-          setValue(true)
-          setName(res.data.name)
-          setProgress(100)
-
-          setTimeout(()=>{
-            history.push('/perfil')
-          }, 800)
-        })
-        .catch((err)=>{
-          setProgress(100)
-          setValue(false)
-          toast.warn('Usuário ou senha inválidos', {
-            position: toast.POSITION.BOTTOM_LEFT
-          })
-      })
   }
+
+  console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID)
   
   return (
     <div className="login-content">
@@ -58,27 +29,8 @@ function LoginPage() {
         progress={progress}
         onLoaderFinished={() => setProgress(0)}></LoadingBar>
         <main className="login-field">
-            <h1>Realize o Login</h1>
-            <form onSubmit={handleLogin} className="login-form">
-                <TextInput
-                  value={email}
-                  handleChange={setEmail}
-                  name="email"
-                  label="E-mail"
-                  isRequired />
-
-                <TextInput
-                  value={password}
-                  handleChange={setPassword}
-                  name="password"
-                  label="Senha"
-                  type="password"
-                  isRequired />
-                <DefaultButton label="Entrar"/>
-            </form>
-            <div className="signin">
-                  <h5>Ou cadastre-se clicando <Link to="/cadastrar">aqui</Link></h5>
-            </div>
+          <h1>Entre com o Google</h1>
+          <GoogleLoginButton text="Accesar com o Google" />
       </main>
     </div>
   );
