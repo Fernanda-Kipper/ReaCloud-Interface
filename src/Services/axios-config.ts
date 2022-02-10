725edb84a73a8576a87433837c4498d5013b07f0
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-02-10 18:36:39 -0300

    feat: add access header to axios config

--- src/Services/axios-config.ts
+++ src/Services/axios-config.ts
@@ -4,7 +4,8 @@ const axios = Axios.create({
     baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3200' : process.env.REACT_APP_API_URL,
     withCredentials: true,
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
     }
 })
 
