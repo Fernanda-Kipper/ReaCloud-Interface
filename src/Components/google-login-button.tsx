569b2d99f8366f79e8fccd26f813c056af71f392
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-02-10 09:56:18 -0300

    fix: change data for env variables

--- src/Components/google-login-button.tsx
+++ src/Components/google-login-button.tsx
@@ -12,8 +12,8 @@ interface Props {
 function getGoogleAuthURL() {
   const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
   const options = {
-    redirect_uri: "http://localhost:3000/auth",
-    client_id: "909104427753-uccm5f130u3pkukhngf8ng8pfeinc3tn.apps.googleusercontent.com",
+    redirect_uri: process.env.REACT_APP_AUTH_REDIRECT_URI,
+    client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
     access_type: "offline",
     response_type: "code",
     prompt: "consent",
