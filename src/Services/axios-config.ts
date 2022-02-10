a74d55387a1d2067613316be264faa78a8d13485
Fernanda Kipper Bucoski de Sousa <nanda.kipper@gmail.com>
2022-02-10 18:44:36 -0300

    fix: axios headers

--- src/Services/axios-config.ts
+++ src/Services/axios-config.ts
@@ -4,8 +4,7 @@ const axios = Axios.create({
     baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3200' : process.env.REACT_APP_API_URL,
     withCredentials: true,
     headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Content-Type': 'application/json'
     }
 })
 
