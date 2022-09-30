d33bfe1e281749d4c06f4eac0035fec57bd96bcd
Fernanda Kipper <nanda.kipper@gmail.com>
2022-09-30 11:21:57 -0300

    fix: transform plugin config in enviroment variable

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const EXTENSION_ID = "flnllibpodbojpadpmpajmggfjchabdp";
+const EXTENSION_ID = process.env.REACT_APP_EXTENSION_ID ?? '';
 const EXTENSION_URL = "https://chrome.google.com/webstore/detail/reacloud/flnllibpodbojpadpmpajmggfjchabdp";
 const GTM_ID = 'GTM-KR75BDK';
 
